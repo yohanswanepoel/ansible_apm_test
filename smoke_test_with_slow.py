8cb4c2ae29f1d1e9864cb4b532edfa5b17bfc353
Johan Swanepoel <johannes.swanepoel@gmail.com>
2022-01-20 10:40:52 +1100

    added a slow playbook on purpose

---
+++ smoke_test_with_slow.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import platform
+
+def run_test(team, x):
+    os.system("export OTEL_RESOURCE_ATTRIBUTES=\"team='{}',manual_effort=25\"; export OTEL_SERVICE_NAME='Demo Test'; ansible-playbook test-apm-with-slow.yaml ; unset OTEL_SERVICE_NAME".format(team))
+
+for x in range(1):
+    run_test("Test A", x)
+    run_test("Test B", x)
