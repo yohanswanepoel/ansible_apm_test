30d1196f821be3854a4281451a02284a4b134e0c
Johan Swanepoel <johannes.swanepoel@gmail.com>
2022-01-20 15:52:08 +1100

    some randomness in test data

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import platform
+import random
+
 
 os.environ['OTEL_EXPORTER_OTLP_ENDPOINT'] = 'http://localhost:8200'
 
@@ -11,8 +13,10 @@ def run_test(team, x):
     if x // 5 == 0:
         os.system("export OTEL_RESOURCE_ATTRIBUTES=\"team='{}',manual_effort=25\"; export OTEL_SERVICE_NAME='Demo Test'; ansible-playbook test-apm-with-error.yaml ; unset OTEL_SERVICE_NAME".format(team))
 
-for x in range(60):
+for x in range(random.randrange(30,60)):
     run_test("Team A", x)
     run_test("Team B", x)
+
+for x in range(random.randrange(30,60)):
     run_test("Team C", x)
     run_test("Team D", x)
