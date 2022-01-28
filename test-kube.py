b989548444c6546ca795aeae55610ffd8d64ce83
Johan Swanepoel <johannes.swanepoel@gmail.com>
2022-01-28 14:51:39 +1100

    refactored to tidy things up

--- test-kube.py
+++ test-kube.py
@@ -2,4 +2,4 @@ import os
 import sys
 import platform
 
-os.system("export OTEL_RESOURCE_ATTRIBUTES=\"team='Platform',manual_effort=120\"; export OTEL_SERVICE_NAME='Petclinic Test Environment'; ansible-playbook test-kube-orchestration.yaml ; unset OTEL_SERVICE_NAME")
+os.system("export OTEL_RESOURCE_ATTRIBUTES=\"team='Platform',manual_effort=120\"; export OTEL_SERVICE_NAME='Petclinic Test Environment'; ansible-playbook kube/test-kube-orchestration.yaml ; unset OTEL_SERVICE_NAME")
