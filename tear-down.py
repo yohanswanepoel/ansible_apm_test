8c707ff0ff65b48489acab09f611740079d1ed23
Johan Swanepoel <johannes.swanepoel@gmail.com>
2022-02-01 08:42:40 +1100

    making teardown more verbose

--- tear-down.py
+++ tear-down.py
@@ -4,8 +4,10 @@ import platform
 
 os.system("ansible-playbook elastic_stack/stop-stack.yaml")
 
+print("Stopping Kubernetes and Removing Profile")
 os.system("minikube delete --profile=apmtest")
 
+print("Verify Stack is down")
 os.system("ps -ef | grep -i elastic | grep -i java")
 os.system("ps -ef | grep -i kibana | grep -i java")
 os.system("ps -ef | grep -i apm-server | grep -i java")
